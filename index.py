eec24b7ca577b8975560956be251e9dd32e064cd
kudou reira <h.i.y.e.o.XD@gmail.com>
2018-03-17 23:30:19 +0900

    playing around with rendering pandas dataframes with jinja

--- index.py
+++ index.py
@@ -1,13 +1,22 @@
-from flask import Flask, abort, request, jsonify
+from flask import Flask, abort, request, jsonify, render_template
 import json, chardet
-import csv
+import os, csv
+import pandas as pd
 
 app = Flask(__name__)
+
+scriptDir = os.path.dirname(os.path.abspath(__file__))
+csvDir = os.path.join(scriptDir, 'outputCSV')
+
+try:
+	os.makedirs(csvDir)
+except OSError:
+	pass # already exists
 
 @app.route('/processBook', methods=['POST'])
 def main(): 
 	# print(request.data)
-	# print(json.loads(data))
+
 	print(type(request.data))
 	received = request.data.decode("utf-8")
 	data = json.loads(received)
@@ -16,7 +25,8 @@ def main():
 	for book in data:
 			tempFilename = book["title"] + "(" + book["creators"] + ")" + ".csv"
 			# print(tempFilename)
-			with open(tempFilename, 'w', newline='', encoding='utf-8-sig') as f:
+			outputCSVFile = os.path.join(csvDir, tempFilename)
+			with open(outputCSVFile, 'w', newline='', encoding='utf-8-sig') as f:
 				fieldnames = ['searchedWord', 'pronounciations', 'englishDefinition', 'partOfSpeech', 'tags', 'seeAlso', 'info']
 				writer = csv.DictWriter(f, fieldnames=fieldnames)
 				writer.writeheader()
@@ -44,13 +54,48 @@ def main():
 							
 							counter += 1
 							entryCounter += 1
+			
+			# should be done writing to csv
+			# now render it to a template
 
+	links = []
+	linkStr = '/outputBooks/'
 
-	return jsonify({'result' : 'success'})
+	directory = os.fsencode(csvDir)
+	for file in os.listdir(directory):
+		filename = os.fsdecode(file)
+		if filename.endswith(".csv"): 
+			# create a list of csv filenames, these will have csv replaced with html(?)
+			# returns a web link
+			# ex. "your files have been created, click here to view them (served) or to download"
+			# print(os.path.join(directory, filename))
+			
+			links.append(request.base_url.replace('/processBook', '') + linkStr + filename.replace('.csv', ''))
+
+	# return a list of rendered views (links, or whatever)
+	print(links)
+	return jsonify({'result' : 'success', 'links': links})
+
+# https://stackoverflow.com/questions/34009980/return-a-download-and-rendered-page-in-one-flask-response
+# https://python-forum.io/Thread-show-csv-file-in-flask-template-html
 
 @app.route('/test')
 def index(): 
 	return "this is the test page"
+
+@app.route('/outputBooks')
+def processedLinks():
+	return 'outputBooks'
+
+@app.route('/outputBooks/<filename>')
+def output_download(filename):
+    # error is file does not exist
+	columns = ["searchedWord", "pronounciations", "englishDefinition", "partOfSpeech", "tags", "seeAlso", "info"]
+	directoryPath = csvDir + "\\" + filename + ".csv"
+	df = pd.read_csv(directoryPath, engine='python')
+	tempHTML = df.to_html()
+	return render_template('index.html', table=tempHTML)
+	# return "this is the filename" + filename + "and this is the directoryPath" + directoryPath
 
 
 if __name__ == '__main__':
--- test.py
+++
@@ -1,7 +0,0 @@
-s = '深入 Python'
-by = s.encode('utf-8')
-print(s)
-print(by)
-
-s = by.decode()
-print(s)
